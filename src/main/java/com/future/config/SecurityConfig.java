84253915cb4942a9fdec82dafdfe51a3f449d32c
DaoXuan <102936237+daoxuan233@users.noreply.github.com>
2023-03-12 00:06:00 +0800

    Modify First

---
+++ src/main/java/com/future/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.future.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 配置 Spring Security 的授权规则和登录和登出的路径。
+     * @param http Http 安全性
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/login").permitAll() //配置 `/login` 路径可以不需要身份认证就可以访问。
+                .anyRequest().authenticated()
+                .and()
+                .formLogin() //配置表单登录
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    /**
+     * 配置身份认证方式，这里使用了一个简单的内存中的用户信息。
+     * @param auth 身份验证管理器生成器
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication() // 配置内存中的用户信息
+                .withUser("user").password("{noop}password").roles("USER");
+    }
+}
